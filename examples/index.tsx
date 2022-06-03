ecd477a67f9d2770246c82d279998e53e2bfae1a
Yohann Rabatel <[omitted]>
2022-06-03 22:48:15 +0200

    up react to 18

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import Examples from './Examples'
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const container = document.getElementById('root')!
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
     <Examples />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
