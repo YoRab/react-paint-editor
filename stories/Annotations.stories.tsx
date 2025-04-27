51c31c0c10bbab6c8d9201f8f617a89d842bc392
Yohann Rabatel <yohann.rabatel@gmail.com>
2025-04-27 13:16:16 +0200

    fox stories imports

--- stories/Annotations.stories.tsx
+++ stories/Annotations.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react/*'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import { type SyntheticEvent, useState } from 'react'
 import { Canvas, Editor, useReactPaint } from '../src/index'
 import './annotations.css'
