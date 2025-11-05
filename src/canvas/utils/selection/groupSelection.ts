7bf8748e361efbe7d1d75bbf5f7908d9d90448ba
Yohann Rabatel <yohann.rabatel@gmail.com>
2025-11-05 17:13:05 +0100

    fix group drawing with rotation

--- src/canvas/utils/selection/groupSelection.ts
+++ src/canvas/utils/selection/groupSelection.ts
@@ -1,5 +1,6 @@
 import type { UtilsSettings } from '@canvas/constants/app'
-import { updateCanvasContext } from '@canvas/utils/canvas'
+import { transformCanvas, updateCanvasContext } from '@canvas/utils/canvas'
+import { getShapeInfos } from '@canvas/utils/shapes'
 import type { DrawableShape } from '@common/types/Shapes'
 
 export const drawSelectionGroup = (
@@ -18,23 +19,31 @@ export const drawSelectionGroup = (
     lineWidth: selectionWidth / settings.canvasSize.scaleRatio
   })
 
+  if (withAnchors && !shape.locked) {
+    ctx.stroke(shape.selection.border)
+    if (shape.selection.line) ctx.stroke(shape.selection.line)
+
+    updateCanvasContext(ctx, {
+      fillColor: 'rgb(255,255,255)',
+      strokeColor: 'rgb(150,150,150)',
+      lineWidth: 2 / settings.canvasSize.scaleRatio
+    })
+
+    for (const anchor of shape.selection.anchors) {
+      ctx.fill(anchor)
+      ctx.stroke(anchor)
+    }
+  }
+
   shape.shapes.forEach(shape => {
+    ctx.restore()
+    updateCanvasContext(ctx, {
+      fillColor: 'transparent',
+      strokeColor: selectionColor,
+      lineWidth: selectionWidth / settings.canvasSize.scaleRatio
+    })
+    const { center } = getShapeInfos(shape, settings)
+    transformCanvas(ctx, settings, shape.rotation, center)
     if (shape.selection?.shapePath) ctx.stroke(shape.selection.shapePath)
   })
-
-  if (!withAnchors || shape.locked) return
-
-  ctx.stroke(shape.selection.border)
-  if (shape.selection.line) ctx.stroke(shape.selection.line)
-
-  updateCanvasContext(ctx, {
-    fillColor: 'rgb(255,255,255)',
-    strokeColor: 'rgb(150,150,150)',
-    lineWidth: 2 / settings.canvasSize.scaleRatio
-  })
-
-  for (const anchor of shape.selection.anchors) {
-    ctx.fill(anchor)
-    ctx.stroke(anchor)
-  }
 }
