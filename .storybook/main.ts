07bdb169320792f7c848b5d07eb043786ca0b0a0
Yohann Rabatel <yohann.rabatel@gmail.com>
2026-01-23 15:19:25 +0100

    up deps and add storybook testing libs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,11 @@ import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
-  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-vitest'
+  ],
 
   framework: {
     name: '@storybook/react-vite',
